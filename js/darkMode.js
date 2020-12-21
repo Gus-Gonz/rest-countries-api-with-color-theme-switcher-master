5d2656953a08fccafc4f050ebe060ade5c7443b1
Gustavo Gonzalez <Gegf1234@Gmail.com>
2020-12-21 19:08:07 -0300

    Added the file for darkMode handler

---
+++ js/darkMode.js
@@ -0,0 +1,6 @@
+const darkModeButton = document.getElementsByClassName('dark-button')[0];
+
+
+darkModeButton.addEventListener('click', () => {
+    console.log('changing to dark Mode .... OH WAIT !! It is under development. ')
+})
